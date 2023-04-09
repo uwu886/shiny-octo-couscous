176cd8a091c6eb2d441ae7193d500e7226bb61b1
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 23:16:22 +0800

    Create VideoList.vue

---
+++ components/VideoList.vue
@@ -0,0 +1,39 @@
+<!-- src/components/VideoList.vue -->
+<template>
+  <div class="video-list">
+    <video-item v-for="video in videos" :key="video.id" :video="video"></video-item>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import VideoItem from './VideoItem.vue';
+
+export default {
+  name: 'VideoList',
+  components: {
+    VideoItem
+  },
+  data() {
+    return {
+      videos: []
+    };
+  },
+  async created() {
+    const response = await axios.get('/api/videos');
+    this.videos = response.data;
+  }
+};
+</script>
+
+<style scoped>
+.video-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.video-item {
+  width: 33.33%;
+  padding: 15px;
+}
+</style>
