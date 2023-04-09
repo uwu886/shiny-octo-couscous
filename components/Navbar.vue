426fc2a219791c47363ee5e599b383c9aa7bc3b2
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 23:14:44 +0800

    Create Navbar.vue

---
+++ components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">首页</router-link>
+      </li>
+      <li>
+        <router-link to="/videos">视频列表</router-link>
+      </li>
+      <li>
+        <router-link to="/upload">上传视频</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
+
+<style scoped>
+nav {
+  background-color: #333;
+  padding: 10px;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+nav ul li {
+  margin-right: 10px;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+</style>
