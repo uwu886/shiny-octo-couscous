cc041113b0c41310cb916782ffa28850db2e8b23
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 23:15:53 +0800

    Create VideoItem.vue

---
+++ components/VideoItem.vue
@@ -0,0 +1,33 @@
+<!-- src/components/VideoItem.vue -->
+<template>
+  <div class="video-item">
+    <img :src="video.thumbnail" :alt="video.title" />
+    <h3>{{ video.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoItem',
+  props: {
+    video: Object
+  }
+};
+</script>
+
+<style scoped>
+.video-item {
+  display: flex;
+  align-items: center;
+}
+
+.video-item img {
+  width: 100px;
+  height: 60px;
+  margin-right: 10px;
+}
+
+.video-item h3 {
+  margin: 0;
+}
+</style>
