f9cad55baa6b21eaac4279a5d7673c9cd2b49cc5
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 23:16:54 +0800

    Create VideoPlayer.vue

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,32 @@
+<!-- src/components/VideoPlayer.vue -->
+<template>
+  <div class="video-player">
+    <video controls :src="videoUrl" @ended="onVideoEnded"></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoPlayer',
+  props: {
+    videoUrl: String
+  },
+  methods: {
+    onVideoEnded() {
+      this.$emit('videoEnded');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.video-player {
+  width: 100%;
+  text-align: center;
+}
+
+.video-player video {
+  max-width: 100%;
+  height: auto;
+}
+</style>
