833f682a61a09fc3e7407141227901689ea3c5fc
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 22:53:43 +0800

    Create VideoDetails.vue

---
+++ 来源/观点/VideoDetails.vue
@@ -0,0 +1,48 @@
+<!-- src/views/VideoDetails.vue -->
+<template>
+  <!-- ... -->
+  <div class="video-info">
+    <!-- ... -->
+  </div>
+  <video-comments :videoId="video.id" />
+</template>
+
+<script>
+import VideoComments from '@/components/VideoComments.vue';
+// ...
+
+export default {
+  // ...
+  components: {
+    VideoComments
+  },
+  // ...
+};
+</script>
+
+<!-- src/views/VideoDetails.vue -->
+<template>
+  <!-- ... -->
+  <div class="video-info">
+    <!-- ... -->
+    <button @click="likeVideo">{{ video.likes }} 赞</button>
+  </div>
+  <!-- ... -->
+</template>
+
+<script>
+// ...
+
+export default {
+  // ...
+  methods: {
+    async likeVideo() {
+      const response = await axios.post(`/api/videos/${this .video.id}/likes`);
+this.video.likes = response.data.likes;
+},
+// ...
+},
+// ...
+};
+</script>
+
