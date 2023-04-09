3ea4498d14fed5e96bcc521f464730079de99948
uwu886 <129765440+uwu886@users.noreply.github.com>
2023-04-09 23:18:11 +0800

    Create CommentItem.vue

---
+++ components/CommentItem.vue
@@ -0,0 +1,29 @@
+<!-- src/components/CommentItem.vue -->
+<template>
+  <div class="comment-item">
+    <div class="comment-author">{{ comment.author }}</div>
+    <div class="comment-content">{{ comment.content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentItem',
+  props: {
+    comment: Object
+  }
+};
+</script>
+
+<style scoped>
+.comment-item {
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+}
+
+.comment-author {
+  font-weight: bold;
+}
+</style>
